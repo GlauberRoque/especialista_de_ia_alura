cb2eb04ebbd008fd2e240182ebb682ce1f519a64
GlauberRoque <80328841+GlauberRoque@users.noreply.github.com>
2025-11-18 18:18:50 -0300

    Update desafio_final.py

--- Inteligencia_artificial_aplicada/desafio_final.py
+++ Inteligencia_artificial_aplicada/desafio_final.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
-
+import json
+from contato_llm import recebe_linha
 
 '''
 Desafio final
@@ -10,17 +11,58 @@ Desafio final
     - Conta a quantidade de avaliação positivas, negativa e neutras
     - une cada item dessa lista em uma variavel do tipo string com algum separador e retorna os dois
 '''
-#
-client_openai = OpenAI(
-    base_url="http://127.0.0.1:1234/v1",
-    api_key="teste_ia",
-)
 
-resposta_do_llm = client_openai.chat.completions.create(
-    model="google/gemma-3-1b",
-    messages=[{"role":"system", "content": "Você é um agente de IA prestativo."},
-              {"role":"user", "content": "O que é IA Generativa?"}],
-    temperature=1.0
-)
 
-print(resposta_do_llm.choices[0].message.content)
+lista_resenhas = []
+#Leitura do arquivo
+with open("Arquivo/Resenhas_App_ChatGPT.txt", "r", encoding="utf-8") as arquivo:
+    for linha in arquivo:
+        lista_resenhas.append(linha.strip())
+        
+#Chama a função que vai retornar um JSON preparado pela LLM e transforma em dicionario
+lista_resenhas_json = []
+for resenha in lista_resenhas:
+    resenha_json = recebe_linha(resenha)
+    # Verificar se a resposta não está vazia
+    if not resenha_json.strip():
+        print(f"Erro: Resposta vazia para a resenha: {resenha}")
+        continue  # Pula para a próxima resenha
+    
+    # Remover cabeçalhos ou rodapés como ```json e ```
+    resenha_json = resenha_json.strip("```json").strip("```").strip()
+    
+    try:
+        resenha_dict = json.loads(resenha_json)
+        lista_resenhas_json.append(resenha_dict)
+    except json.JSONDecodeError as e:
+        print(f"Erro ao decodificar JSON para a resenha: {resenha}")
+        print(f"Resposta recebida: {resenha_json}")
+        print(f"Erro: {e}")
+    
+#print(lista_resenhas_json)    
+
+def Contador_e_juntador(lista_de_dicionarios):
+    contador_positiva = 0
+    contador_negativa = 0
+    contador_neutra = 0
+    lista_de_dicionarios_str = []
+    
+    for item in lista_de_dicionarios:
+        
+        if item['avaliacao'] == "positiva":
+            contador_positiva += 1
+        elif item['avaliacao'] == "negativa":
+            contador_negativa += 1
+        elif item['avaliacao'] == "neutra":
+            contador_neutra += 1
+        
+        lista_de_dicionarios_str.append(str(item))
+        
+    textos_unidos = "#####".join(lista_de_dicionarios_str)
+    return contador_positiva, contador_negativa, contador_neutra, textos_unidos
+
+pos, neg, neut, textos = Contador_e_juntador(lista_resenhas_json)
+print(f"Positivas: {pos}\n")
+print(f"Negativas: {neg}\n")
+print(f"Neutras: {neut}\n")
+print(f"Textos: {textos}\n")
